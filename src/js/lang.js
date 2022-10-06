232ac1624e024e06bcc1ec4a714ec6cbd4b0c38f
ponidevito <olegmama@gmail.com>
2022-10-06 18:24:59 +0300

    master

---
+++ src/js/lang.js
@@ -0,0 +1,92 @@
+"use strict";
+
+const langArr = {
+  'unit': {
+    en: "CV",
+    es: "Curriculum",
+    ua: "Резюме",
+  },
+  'name': {
+    en: "Kravchuk Oleh",
+    es: "Kravchuk Oleh",
+    ua: "Кравчук Олег",
+  },
+  'profession': {
+    en: "Web-developer",
+    es: "Desarrollador web",
+    ua: "Веб-розробник",
+  },
+  'age': {
+    en: "Age",
+    es: "Años",
+    ua: "Вік",
+  },
+
+  /*   adress       */
+  'adress': {
+    en: "Adress",
+    es: "Direccion",
+    ua: "Адреса",
+  },
+
+  'adress-name': {
+    en: "Lviv Doroshenka st 63",
+    es: "Lviv c/Doroshenka 63",
+    ua: "м.Львів Дорошенка 63",
+  },
+
+  /*   adress       */
+
+  'phone': {
+    en: "Phone",
+    es: "Telefono",
+    ua: "Телефон",
+  },
+
+  'email': {
+    en: "Email",
+    es: "Correo electronico",
+    ua: "Ел.пошта",
+  },
+  'skills': {
+    en: "Skills",
+    es: "Habilidades",
+    ua: "Навички",
+  },
+
+   /*   about       */
+  'about': {
+    en: "About me",
+    es: "Sobre mi",
+    ua: "Про мене",
+  },
+
+  'about-text': {
+    en: "Hello! My name is Oleg. I am 31 years old. I am punctual and responsible for my work, I am ready to learn something new and develop in this area in your team!",
+    es: "Hola! Mi nombre es Oleh ! Tengo 31 años. Soy puntual y responsable de mi trabajo.¡Estoy listo para aprender algo nuevo y desarrollarme en este campo en su equipo!",
+    ua: "Привіт! Мене звати Олег! Мені 31 рік. Я пунктуальний та відповідально ставлюся до своєї роботи. Я готовий навчатися чомусь новому та розвиватись у цій сфері у вашій команді!",
+  },
+   /*   about       */
+
+   // contacts 
+
+   'contacts': {
+    en: "Contacts",
+    es: "Contactos",
+    ua: "Контакти",
+  },
+
+  
+  'phone-contacts': {
+    en: "Phone:",
+    es: "Telefono:",
+    ua: "Телефон:",
+  },
+
+  'email-contacts': {
+    en: "Email:",
+    es: "Correo electronico:",
+    ua: "Ел.пошта:",
+  },
+
+};
