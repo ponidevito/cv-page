e1efc96a62e618f2564aab28c310d0f577673dde
ponidevito <olegmama@gmail.com>
2023-01-18 19:21:08 +0200

    gsap

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,18 +1,60 @@
 // Аккордеон
 function accordion() {
-    const items = document.querySelectorAll('.accordion__item-trigger')
-    items.forEach(item => {
-        item.addEventListener('click', () => {
-            const parent = item.parentNode
-            if (parent.classList.contains('accordion__item-active')) {
-                parent.classList.remove('accordion__item-active')
-            } else {
-                document
-                    .querySelectorAll('.accordion__item')
-                    .forEach(child => child.classList.remove('accordion__item-active'))   
-                parent.classList.add('accordion__item-active')
-            }
-        })
-    })
+  const items = document.querySelectorAll(".accordion__item-trigger");
+  
+  items.forEach((item,index) => {
+  
+    item.addEventListener("click", () => {
+      const parent = item.parentNode;
+      if (parent.classList.contains("accordion__item-active")) {
+        parent.classList.remove("accordion__item-active");
+          gsap.set(parent, {  });
+        // parent.style.backgroundColor = "#dde4ce";
+      } else {
+        // parent.style.backgroundColor = "rgb(233 243 211 / 38%)";
+       
+        document.querySelectorAll(".accordion__item").forEach((child) => child.classList.remove("accordion__item-active"));
+        // document.querySelectorAll(".accordion__item").forEach((item) => item.style.backgroundColor = "rgb(233 243 211 / 38%)");
+        parent.classList.add("accordion__item-active");
+        // parent.style.backgroundColor = "rgb(233 243 211 / 38%)";
+      }
+    });
+  });
 }
-accordion() 
+accordion();
+
+// const list = gsap.utils.toArray(".accordion");
+
+// list.forEach((accordion, index) => {
+//   let isOpen = index === 0 ? true : false;
+
+//   const title = accordion.getElementsByClassName("accordion__item-trigger")[0];
+//   // const icon = accordion.getElementsByClassName("accordion__icon")[0];
+//   const content = accordion.getElementsByClassName(
+//     "accordion__item-content"
+//   )[0];
+
+//   gsap.set(
+//     title,
+//     isOpen
+//       ? { background: "#fff", color: "#000" }
+//       : { background: "#4b0082", color: "#fff" }
+//   );
+//   gsap.set(content, { height: isOpen ? "auto" : "0px"});
+//   gsap.set(content, {  display: isOpen ? "block" : 'nonne' });
+
+//   // gsap.set(icon, { scale: isOpen ? -1 : 1 });
+
+//   title.addEventListener("click", () => {
+//     isOpen = !isOpen;
+
+//     gsap.to(
+//       title,
+//       isOpen
+//         ? { background: "#fff", color: "#000" }
+//         : { background: "#4b0082", color: "#fff" }
+//     );
+//     gsap.to(content, { height: isOpen ? "auto" : "0px" });
+//     // gsap.to(icon, { scale: isOpen ? -1 : 1 });
+//   });
+// });
--- src/js/main.js
+++ src/js/main.js
@@ -22,7 +22,7 @@ let select = function () {
   selectHeader.addEventListener("click", selectToggle);
 
   function selectToggle() {
-    selectBody.classList.add("opacity");
+    selectBody.classList.toggle("opacity");
     event.stopPropagation();
   }
 
@@ -45,12 +45,13 @@ let select = function () {
 
 select();
 
-window.addEventListener("load", function () {
+window.addEventListener("load", function () { 
   if (localStorage.getItem("place1"))
     document.querySelector(".select__current").innerHTML =
       localStorage.getItem("place1");
 });
 
+// function change lenguage
 function changeLanguage() {
   let hash = window.location.hash;
   hash = hash.substring(1);
@@ -59,6 +60,7 @@ function changeLanguage() {
     location.href = window.location.pathname + "#en";
     location.reload();
   }
+  
   document.querySelector("title").innerHTML = langArr["unit"][hash];
   for (let key in langArr) {
     let elem = document.querySelector(".lng-" + key);
@@ -70,10 +72,45 @@ function changeLanguage() {
 changeLanguage();
 
 
-
+// html or body close click 
 html.addEventListener("click", function (e) {
   if (e.target.tagName !== "HTML" || e.target.tagName !== "BODY") {
     selectBody.classList.remove("opacity");
-    
   }
 });
+
+
+
+
+// gsap
+
+//header
+gsap.fromTo(".header__image", {opacity: 0}, {opacity: 1, duration: 0.3});
+gsap.fromTo(".title", {x:400,opacity:0}, {x:0,opacity: 1, duration: 1.1});
+gsap.fromTo(".header__lenguage", {x:400,opacity:0}, {x:0,opacity: 1, duration: 1.2});
+gsap.fromTo(".lng-age", {x:400,opacity:0}, {x:0,opacity: 1, duration: 1.3});
+gsap.fromTo(".textInfo-age", {x:400,opacity:0}, {x:0,opacity: 1, duration: 1.3});
+gsap.fromTo(".lng-adress", {x:400,opacity:0}, {x:0,opacity: 1, duration: 1.4});
+gsap.fromTo(".lng-adress-name", {x:400}, {x:0, duration: 1.4,});
+gsap.fromTo(".lng-phone", {x:400,opacity:0}, {x:0,opacity: 1, duration: 1.5});
+gsap.fromTo(".textInfoPhone", {x:400,opacity:0}, {x:0,opacity: 1, duration: 1.5});
+gsap.fromTo(".lng-email", {x:400,opacity:0}, {x:0,opacity: 1, duration: 1.6});
+gsap.fromTo(".textInfoEmail", {x:400,opacity:0}, {x:0,opacity: 1, duration: 1.6});
+
+//accrodeon
+gsap.fromTo(".accordion", {y:200,opacity:0}, {y:0,opacity: 1, duration: 1.5});
+gsap.fromTo(".accordion__item-trigger", {opacity:0}, {opacity: 1, duration: 2});
+
+
+
+//footer
+
+gsap.from(".footer", {opacity:0,delay:1.8});
+
+
+
+
+
+
+
+
